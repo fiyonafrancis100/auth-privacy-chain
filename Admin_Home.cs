c1a70f788274316dd9145d8e8f40d807993c0e51
fiyonafrancis100 <82795816+fiyonafrancis100@users.noreply.github.com>
2022-05-28 12:11:40 +0530

    Add files via upload

---
+++ Admin_Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SecureTransferMI
+{
+    public partial class Admin_Home : Form
+    {
+        public Admin_Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
